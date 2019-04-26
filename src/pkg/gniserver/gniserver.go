5befb194f23ae1863a21825e202103f9c85f3852
adibrastegarnia <arastega@purdue.edu>
2019-04-26 00:50:57 -0400

    gniserver has been added(WIP)

--- src/pkg/gniserver/gniserver.go
+++ src/pkg/gniserver/gniserver.go
@@ -1,15 +1,40 @@
-package main
+package gniserver
 
 import (
-	"github.com/gniproject/gni_prototype/src/api/gni"
+	"context"
+	"log"
+
+	gni "github.com/gniproject/gni_prototype/src/api/gni"
 )
 
+const (
+	port = ":50051"
+)
 
-type gniServer struct {
+type GniServer struct{}
 
+func (s *GniServer) Fetch(ctx context.Context, req *gni.FetchRequest) (*gni.FetchResponse, error) {
+
+	log.Println("Test")
+
+	return &gni.FetchResponse{}, nil
 }
 
-func (*s gniServer) Fetch(context.Context, *FetchRequest) (*FetchResponse, error) {
+func (s *GniServer) Store(ctx context.Context, req *gni.StoreRequest) (*gni.StoreResponse, error) {
 
+	log.Println("Test")
+
+	return &gni.StoreResponse{}, nil
 }
 
+func (s *GniServer) Command(ctx context.Context, req *gni.CommandRequest) (*gni.CommandResponse, error) {
+
+	log.Println("Test")
+
+	return &gni.CommandResponse{}, nil
+}
+
+func (s *GniServer) Stream(streamServer gni.GNI_StreamServer) error {
+
+	return nil
+}
